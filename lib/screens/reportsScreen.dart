bee55c69c2b5138204b3c0a86e6ef46ec232292d
Arnav Rustagi <arnav.rustagi@plaksha.edu.in>
2024-09-15 23:24:19 +0530

    feat: added reports UI

--- lib/screens/reportsScreen.dart
+++ lib/screens/reportsScreen.dart
@@ -1,31 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kavach/components/main_component.dart';
+import 'package:kavach/screens/mainScreen.dart';
 
-Container make_report_widget(String name, int phone_number) {
-  return Container(
-    decoration: BoxDecoration(
-      border: Border(
-        top: BorderSide(color: Colors.black, width: 2),
-        right: BorderSide(color: Colors.black, width: 2),
-        bottom: BorderSide(color: Colors.black, width: 2),
-        left: BorderSide(color: Colors.black, width: 2),
-      ),
-      borderRadius: BorderRadius.circular(16.0),
-    ),
-    child: Padding(
-      padding: EdgeInsets.only(top: 8.0, bottom: 8.0, left: 18.0, right: 18.0),
-      child: Row(
-        children: [
-          Text(name, style: TextStyle(fontWeight: FontWeight.bold)),
-          Spacer(),
-          Text("$phone_number"),
-        ],
-      ),
-    ),
-  );
-}
-
-
+final List<String> events = fabData.map((event) => event.text).toList();
 
 class ReportsScreen extends StatefulWidget {
   const ReportsScreen({super.key});
@@ -36,17 +13,139 @@ class ReportsScreen extends StatefulWidget {
 
 class _ReportsState extends State<ReportsScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  List<DropdownMenuItem<String>> simplified_events = events.map((String value) {
+                return DropdownMenuItem<String>(
+                    value: value, child: Text(value));
+              }).toList();
   List<Widget> _report_widgets = [];
+  String dropdownValue = events.first;
+  bool chosen = false;
 
   @override
   Widget build(BuildContext context) {
+    const space = const SizedBox(
+      width: 12,
+      height: 12,
+    );
+
+    const half_space = const SizedBox(
+      width: 8,
+      height: 8,
+    );
     const red = const Color(0xFFFF6666);
+    const gray = const Color(0xFF242829);
     const green = const Color(0xFF95F6A7);
     const hero_icon = const Icon(
       Icons.report,
       color: red,
       size: 192.0,
     );
+
+    void editReport() {
+      print("hey!");
+      showModalBottomSheet<void>(
+        // context and builder are
+        // required properties in this widget
+        context: context,
+        builder: (BuildContext context) {
+          const border_styling = const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16.0)),
+            borderSide: BorderSide(color: Colors.black, width: 2.0),
+          );
+
+          const report_location = const TextField(
+            decoration: InputDecoration(
+              labelText: "Report location",
+              enabledBorder: border_styling,
+              focusedBorder: border_styling,
+            ),
+          );
+          const additional_details = const TextField(
+            decoration: InputDecoration(
+              labelText: "Additional Details",
+              enabledBorder: border_styling,
+              focusedBorder: border_styling,
+            ),
+          );
+
+          print("isee");
+          print(chosen);
+          DropdownButton incident_type = DropdownButton(
+              items: simplified_events,
+              isExpanded: true,
+              hint: Text("  "+dropdownValue),
+              onChanged: (newValue) {
+                        setState(() {
+                        dropdownValue = newValue;
+                        chosen = true;
+                        });
+                    });
+              
+          Container dropdown_wrapper = Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(color: Colors.black, width: 2),
+                right: BorderSide(color: Colors.black, width: 2),
+                bottom: BorderSide(color: Colors.black, width: 2),
+                left: BorderSide(color: Colors.black, width: 2),
+              ),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: DropdownButtonHideUnderline(child: incident_type));
+
+          return SizedBox(
+              height: 480,
+              child: SizedBox(
+                  height: 256,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Editing Report',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 32)),
+                        space,
+                        report_location,
+                        space,
+                        additional_details,
+                        space,
+                        dropdown_wrapper,
+                      ],
+                    )),
+                  )));
+        },
+      );
+    }
+
+    GestureDetector make_report_widget(String location, String date) {
+      return GestureDetector(
+          onTap: editReport,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(color: Colors.black, width: 2),
+                right: BorderSide(color: Colors.black, width: 2),
+                bottom: BorderSide(color: Colors.black, width: 2),
+                left: BorderSide(color: Colors.black, width: 2),
+              ),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: 8.0, bottom: 8.0, left: 18.0, right: 18.0),
+              child: Row(
+                children: [
+                  Text(location, style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text(date),
+                ],
+              ),
+            ),
+          ));
+    }
+
     const heading_text = const Text(
       "My Reports",
       textAlign: TextAlign.center,
@@ -59,15 +158,17 @@ class _ReportsState extends State<ReportsScreen> {
       style: TextStyle(fontSize: 16),
     );
 
-    const space = const SizedBox(
-      width: 12,
-      height: 12,
-    );
-
-    const half_space = const SizedBox(
-      width: 8,
-      height: 8,
-    );
+    void _update_report_widgets() {
+      setState(() {
+        const half_space = const SizedBox(
+          width: 8,
+          height: 8,
+        );
+        _report_widgets.addAll(
+          <Widget>[make_report_widget("India Gate", "02-09-2024"), half_space],
+        );
+      });
+    }
 
     SizedBox add_report_button = SizedBox(
       width: double.infinity,
@@ -120,19 +221,4 @@ class _ReportsState extends State<ReportsScreen> {
       ),
     );
   }
-
-void _update_report_widgets() {
-    setState( () { 
-      const half_space = const SizedBox(
-      width: 8,
-      height: 8,
-    );
-      _report_widgets.addAll(
-        <Widget>[
-          make_report_widget("Pranjal Rastogi", 9910708969),
-          half_space
-        ],
-      );
-    });
-}
 }
